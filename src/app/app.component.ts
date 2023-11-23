91f63b9a222ebdf65d333e984d9a007d195f3fae
Erasmo Sena <erasmo.sena@gmail.com>
2023-11-23 20:17:15 -0300

    mes por seleção e eventos com mais detalhes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,31 +11,70 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'Eventos';
   colunas: string[] = ['domingo', 'segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado'];
-  meses: string[] = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho','Agosto', 'Setembro', 'Outubro','Novembro', 'Dezembro'];
+  meses = [
+    {
+      codigo: 0, descricao: 'Janeiro'
+    },
+    {
+      codigo: 1, descricao: 'Fevereiro'
+    },
+    {
+      codigo: 2, descricao: 'Março'
+    },
+    {
+      codigo: 3, descricao: 'Abril'
+    },
+    {
+      codigo: 4, descricao: 'Maio'
+    },
+    {
+      codigo: 5, descricao: 'Junho'
+    },
+    {
+      codigo: 6, descricao: 'Julho'
+    },
+    {
+      codigo: 7, descricao: 'Agosto'
+    },
+    {
+      codigo: 8, descricao: 'Setembro'
+    },
+    {
+      codigo: 9, descricao: 'Outubro'
+    },
+    {
+      codigo: 10, descricao: 'Novembro'
+    },
+    {
+      codigo: 11, descricao: 'Dezembro'
+    }];
   diasDoMes = [new Array(7), new Array(7), new Array(7), new Array(7), new Array(7)];
-  mes:number = 0 ;
+  
+  mes: number = 0;
   url: string = '/assets/eventos.json';
-  eventos:any;
+  eventos: any;
   dataAtual = moment(new Date());
   dataAtualFormatada = this.dataAtual.format("DD/MM/YYYY");
 
-  constructor(private http: HttpClient) {}
-  
+  constructor(private http: HttpClient) { }
+
 
 
   ngOnInit(): void {
     this.http.get(this.url).subscribe(res => {
       this.eventos = res;
     });
-  
-    
-    this.mes = moment(this.dataAtual).month();
+    this.montarCalendario();
+  }
+
+  montarCalendario(){
+    //this.mes = 10 ;
     let data = moment(new Date(this.dataAtual.year(), this.mes, 1));
     let semana = 0;
     for (let i = 0; data.month() <= this.mes && i <= 30; i++) {
       let dataFormatada = moment(data).format("DD/MM/YYYY");
-      if( data.weekday()== 0 ){
-        semana+=1;
+      if (data.weekday() == 0) {
+        semana += 1;
       }
       let diaSemana = data.weekday();
       this.diasDoMes[semana][diaSemana] =
@@ -44,14 +83,30 @@ export class AppComponent implements OnInit {
         dia: diaSemana,
         data: dataFormatada
       };
+      // console.log(`[${semana},${diaSemana}]= ${dataFormatada}`)
+      // console.log({
+      //   semana: semana,
+      //   dia: diaSemana,
+      //   data: dataFormatada
+      // });
       data.add(1, 'days');
     }
   }
 
-  obterEvento(data:string){
-    return this.eventos.filter((it:any)=> it.data ==data)
+  obterImagem(path:string){
+    console.log(path);
+    return this.http.get(path);
   }
 
+  obterEvento(data: string) {
+    return this.eventos.filter((it: any) => it.data == data)
+  }
+
+  setPlan($evento:any){
+    this.mes = $evento.target.value;
+    this.diasDoMes = [new Array(7), new Array(7), new Array(7), new Array(7), new Array(7)];
+    this.montarCalendario();
+  }
 
 
 
