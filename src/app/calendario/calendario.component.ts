26538b16de81e7e6b9ad8759bc1116d78ca415ec
Erasmo Sena <erasmo.sena@gmail.com>
2024-01-12 09:41:12 -0300

    ajustando css

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -41,6 +41,7 @@ export class CalendarioComponent implements OnInit, AfterViewInit, OnDestroy {
   viewDate: Date = new Date();
   locale: string = 'pt-BR';
   url: string = `${this.API_URL}/eventos.json`;
+  urlImage: string =  `${this.API_URL}images/`;
   events: CalendarEvent[] = [];
   calendarEvents: any;
   activeDayIsOpen = false;
@@ -64,7 +65,7 @@ export class CalendarioComponent implements OnInit, AfterViewInit, OnDestroy {
           );
           console.log(this.calendarEvents[i].evento.titulo);
           this.calendarEvents[i].title = this.calendarEvents[i].evento.titulo;
-          this.calendarEvents[i].imagem = this.calendarEvents[i].evento.imagem;
+          this.calendarEvents[i].imagem = `${this.urlImage}${this.calendarEvents[i].evento.imagem}`;
         }
         this.events = this.calendarEvents;
       }
